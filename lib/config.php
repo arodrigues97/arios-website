bde0a6430210b1981b7ad3ad985a2a65e40589c7
Adam Rodrigues <36482887+arodrigues97@users.noreply.github.com>
2021-03-20 13:18:59 -0400

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -1,19 +1,19 @@
 <?php 
-define("BASE_TITLE", "WildScape Pk The #1 2009 HD/OSRS Private Server");
+define("BASE_TITLE", "Arios #1 2009 HD/OSRS Private Server");
 define("DESCRIPTION", "The #1 2009 styled RSPS. A unique fast paced MMORPG offering an endless supply of game content.");
-define("KEYWORDS", "wild-scape, wildscape-pk, arios, arios rsps, arios498, arios530, ARIOS, arios, OSRS, oldschool, status,online,play,playnow,eoc,pre eoc,easyplay,community,private,server,rsps,rsps toplist,toplist,rs,arios, Arios RSPS, arios 498, 498, fatality,matrixftw,matrix,rs2006,2006scape,jagex,rsps,soulsplit,ss,near-reality,battlescape,slashscape,runemyth,helix-world,corruptionx,revolutionx,inception,pwnxile,legacy,silabgarza,estrania,ikov,pkhonor,spawn,alotic,blurite,torva,nex");
+define("KEYWORDS", "arios, arios rsps, arios498, arios530, ARIOS, arios, OSRS, oldschool, status,online,play,playnow,eoc,pre eoc,easyplay,community,private,server,rsps,rsps toplist,toplist,rs,arios, Arios RSPS, arios 498, 498, fatality,matrixftw,matrix,rs2006,2006scape,jagex,rsps,soulsplit,ss,near-reality,battlescape,slashscape,runemyth,helix-world,corruptionx,revolutionx,inception,pwnxile,legacy,silabgarza,estrania,ikov,pkhonor,spawn,alotic,blurite,torva,nex")
 define("DB_HOST", "localhost");
-define("DB_USER", "wildscape_user");
-define("DB_PASS", "2273671Wildscapepk");
+define("DB_USER", "");
+define("DB_PASS", "");
 define("DB_NAME", "global");
 define("TWITTER", "https://twitter.com/AriosOfficial");
 define("YOUTUBE", "https://www.youtube.com/channel/UCvPIEVHMn1Yb5CjUoqIBUfw");
-define("MAIL_HOST", "n1plcpnl0037.prod.ams1.secureserver.net");
-define("EMAIL", "wildscapepk@wildscape-pk.com");
-define("EMAIL_PASS", "2273671Timonil");
+define("MAIL_HOST", "");
+define("EMAIL", "");
+define("EMAIL_PASS", "");
 define("ACTION_PATH", "/lib/sys/ActionHandler.php?");
-define("VALIDATE_LINK", "http://wildscape-pk.com/validation/index.php?code=");
-define("PAYPAL_EMAIL", "dani88_d@yahoo.com");
+define("VALIDATE_LINK", "");
+define("PAYPAL_EMAIL", "");
 define("SYSTEM_LOG", 1);
 define("SECURITY_LOG", 2);
 define("DONATION_LOG", 3);
